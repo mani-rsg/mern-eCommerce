466fb95fb5c540b09ff500220e08a4f5d417441b
manirsg <manirsg16@gmail.com>
2021-05-23 21:50:53 +0530

    Update: updated port with env file

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,21 @@
 const express = require('express');
-const app = express();
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
+
+const app = express();
+
 app.get('/', (req, res) => {
     res.send('API is running....!');
 })
 app.get('/api/products', (req, res) => {
     res.json(products);
 })
-app.get('/api/products/:id', (req, res)=>{
-    const product = products.find(product=>product._id===req.params.id);
+app.get('/api/products/:id', (req, res) => {
+    const product = products.find(product => product._id === req.params.id);
     res.json(product);
 })
-app.listen(5000, console.log('server running on 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
