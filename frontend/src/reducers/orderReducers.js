68cb2709726d3523a3eb02e52122d1c097b29f21
manirsg <manirsg16@gmail.com>
2021-07-23 22:11:42 +0530

    Fix: profile page logout fix

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_LIST_MY_FAIL, ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from "../constants/orderConstants"
+import { ORDER_CREATE_FAIL, ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_DETAILS_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_LIST_MY_FAIL, ORDER_LIST_MY_REQUEST, ORDER_LIST_MY_RESET, ORDER_LIST_MY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_RESET, ORDER_PAY_SUCCESS } from "../constants/orderConstants"
 
 export const orderCreateReducer = (state = {}, action) => {
     switch (action.type) {
@@ -27,9 +27,9 @@ export const orderDetailsReducer = (state = { loading: true, orderItems: [], shi
 export const orderPayReducer = (state = {}, action) => {
     switch (action.type) {
         case ORDER_PAY_REQUEST:
-            return {loading: true }
+            return { loading: true }
         case ORDER_PAY_SUCCESS:
-            return { loading: false, success:true }
+            return { loading: false, success: true }
         case ORDER_PAY_FAIL:
             return { loading: false, error: action.payload }
         case ORDER_PAY_RESET:
@@ -38,14 +38,16 @@ export const orderPayReducer = (state = {}, action) => {
             return state
     }
 }
-export const orderListMyReducer = (state = {orders:[]}, action) => {
+export const orderListMyReducer = (state = { orders: [] }, action) => {
     switch (action.type) {
         case ORDER_LIST_MY_REQUEST:
-            return {loading: true }
+            return { loading: true }
         case ORDER_LIST_MY_SUCCESS:
-            return { loading: false, orders:action.payload }
+            return { loading: false, orders: action.payload }
         case ORDER_LIST_MY_FAIL:
             return { loading: false, error: action.payload }
+        case ORDER_LIST_MY_RESET:
+            return { orders: [] }
         default:
             return state
     }
