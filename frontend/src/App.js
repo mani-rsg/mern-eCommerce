05e0480c99eed8ea96fc2a60ad53e036315997dd
manirsg <manirsg16@gmail.com>
2021-07-24 12:44:44 +0530

    New: user list view in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,6 +13,7 @@ import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/paymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import OrderScreen from './screens/OrderScreen';
+import UserListScreen from './screens/UserListScreen';
 const App = () => {
   return (
     <Router>
@@ -28,6 +29,7 @@ const App = () => {
           <Route path='/profile' component={ProfileScreen} />
           <Route path='/products/:id' component={ProductScreen} />
           <Route path='/cart/:id?' component={CartScreen} />
+          <Route path='/admin/userlist' component={UserListScreen} />
           <Route path='/' component={HomeScreen} exact />
         </Container>
       </main>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -36,7 +36,20 @@ const Header = () => {
                                     <Nav.Link><i className="fas fa-user"></i> Sign In</Nav.Link>
                                 </LinkContainer>
                             )}
-
+                            {userInfo && userInfo.isAdmin && (
+                                 <NavDropdown title='Admin' id='adminmenu'>
+                                 <LinkContainer to='/admin/userlist'>
+                                     <NavDropdown.Item>Users</NavDropdown.Item>
+                                 </LinkContainer>
+                                 <LinkContainer to='/admin/productlist'>
+                                     <NavDropdown.Item>Products</NavDropdown.Item>
+                                 </LinkContainer>
+                                 <LinkContainer to='/admin/orderlist'>
+                                     <NavDropdown.Item>Order</NavDropdown.Item>
+                                 </LinkContainer>
+                                 <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                             </NavDropdown>
+                            )}
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
