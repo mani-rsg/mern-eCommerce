e5ed1dc07ed3ed92443f5aa7109eeb23d58289c9
manirsg <manirsg16@gmail.com>
2021-07-25 16:43:38 +0530

    New: add review api

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { createProduct, deleteProduct, getProducts, getProductsById, updateProduct } from '../controllers/productControllers.js'
+import { createProduct, createProductReview, deleteProduct, getProducts, getProductsById, updateProduct } from '../controllers/productControllers.js'
 import { admin, protect } from '../middleware/authMiddelware.js';
 
 router.route('/').get(getProducts).post(protect, admin, createProduct)
-
+router.route('/:id/reviews').post(protect, createProductReview)
 router.route('/:id').get(getProductsById).delete(protect, admin, deleteProduct).put(protect, admin, updateProduct)
 
 export default router;
