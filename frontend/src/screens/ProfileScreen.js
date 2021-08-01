b24acd70ebd340acb249c245cfa02acb02b03e40
manirsg <manirsg16@gmail.com>
2021-08-01 21:10:50 +0530

    Fix: profile name change nav bar

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -6,6 +6,7 @@ import Message from '../components/Message';
 import Loader from '../components/Loader';
 import { getUserDetails, updateUserProfile } from '../actions/userActions';
 import { listMyOrders } from '../actions/orderActions';
+import { USER_UPDATE_PROFILE_RESET } from '../constants/userConstants';
 
 
 
@@ -33,7 +34,8 @@ const ProfileScreen = ({ location, history }) => {
             history.push('/login');
         }
         else {
-            if (!user.name) {
+            if (!user || !user.name || success) {
+                dispatch({ type: USER_UPDATE_PROFILE_RESET })
                 dispatch(getUserDetails('profile'));
                 dispatch(listMyOrders())
             }
@@ -42,7 +44,7 @@ const ProfileScreen = ({ location, history }) => {
                 setEmail(user.email);
             }
         }
-    }, [dispatch, history, userInfo, user])
+    }, [dispatch, history, userInfo, user, success])
     const submitHandler = (e) => {
         e.preventDefault();
         if (password !== confirmPassword) {
